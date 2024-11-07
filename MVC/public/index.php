b0ddc0c5e3fbf3af6e06e9c92eba08287bc20ee3
Mahjabeen Akhtar <themahibabe@hotmail.com>
2024-11-07 14:46:23 +0500

    creating instances of app class

--- MVC/public/index.php
+++ MVC/public/index.php
@@ -1,2 +1,9 @@
-<?php
- 
+<?php 
+
+session_start();
+require "../app/core/init.php";
+
+(DEBUG) ? ini_set('display_errors', 1) :  ini_set('display_errors', 0);
+
+$app= new App();  // Create a new instance of the App class
+$app->loadController();  // Call the loadController method
