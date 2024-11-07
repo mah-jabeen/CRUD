c6349dbeffe991ff3cb51809b6fc78e41bb7c3e8
Mahjabeen Akhtar <themahibabe@hotmail.com>
2024-11-07 17:13:12 +0500

    performing queries

--- MVC/app/core/Database.php
+++ MVC/app/core/Database.php
@@ -6,3 +6,36 @@ Trait Database{
         $con=new PDO($string, DBUSER,DBPASS);
         return $con;
     }
+    public function query($query, $data= [] )
+    {
+            $con= $this->connect();
+            $stmt = $con->prepare($query);
+            $check=$stmt->execute($data);
+            if($check)
+            {
+                $result= $stmt->fetchAll(PDO::FETCH_OBJ);
+            if(is_array($result) && count($result))
+        { 
+            return $result;
+    
+            }        
+    } return false;
+}
+public function get_row($query, $data= [] )
+{
+        $con= $this->connect();
+        $stmt = $con->prepare($query);
+        $check=$stmt->execute($data);
+        if($check)
+        {
+            $result= $stmt->fetchAll(PDO::FETCH_OBJ);
+        if(is_array($result) && count($result))
+    { 
+        return $result[0];
+
+        }        
+} return false;
+}
+    // Generate the model file after creating the table
+    
+}
